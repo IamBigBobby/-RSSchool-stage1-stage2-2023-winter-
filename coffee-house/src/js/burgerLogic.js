fd8a80b674cc8d986922d9edf31b1de9c44c32f8
Denis Svetleishii <denissvetleishii@outlook.com>
2023-12-03 20:46:43 +0400

    feat: add burger functionality

--- coffee-house/src/js/burgerLogic.js
+++ coffee-house/src/js/burgerLogic.js
@@ -1,12 +1,35 @@
-export function burgerLogic() {
-    console.log('heh');
-    const burgerMenu = document.querySelector('.burger-menu');
     const burgerMenuUpSpan = document.querySelector('.burger-menu span:first-child');
-    const burgerMenuDownSpan = document.querySelector('.burger-menu span:last-child')
+    const burgerMenuDownSpan = document.querySelector('.burger-menu span:last-child');
+    const headerLinks = document.querySelector('.header__links');
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navigationLinks = document.querySelectorAll('.navigation__link');
 
-    burgerMenu.addEventListener('click', function() {
-        burgerMenuUpSpan.classList.toggle('burger-menu__first-span');
-        burgerMenuDownSpan.classList.toggle('burger-menu__second-span');
-    })
+export function burgerLogic() {
+    burgerMenu.addEventListener('click', moveBurger);
+    navigationLinks.forEach((link) => {
+        link.addEventListener('click', selectLink);
+    });
+}
+
+function moveBurger() {
+    burgerMenuUpSpan.classList.toggle('burger-menu__first-span');
+    burgerMenuDownSpan.classList.toggle('burger-menu__second-span');
+
+    headerLinks.classList.toggle('header__links_hide');
+
+    if (document.body.style.overflow === 'hidden') {
+        document.body.style.overflow = '';
+    } else {    
+        document.body.style.overflow = 'hidden';
+    }
+}
+
+function selectLink() {
+    burgerMenuUpSpan.classList.toggle('burger-menu__first-span');
+    burgerMenuDownSpan.classList.toggle('burger-menu__second-span');
+
+    headerLinks.classList.toggle('header__links_hide');
+
+    document.body.style.overflow = '';
 }
 burgerLogic();
