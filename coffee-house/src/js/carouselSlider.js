29c6541e9a5f81722266a290c4151925ae13a1f7
Denis Svetleishii <denissvetleishii@outlook.com>
2023-12-05 20:31:29 +0400

    feat: add pagination logic

--- coffee-house/src/js/carouselSlider.js
+++ coffee-house/src/js/carouselSlider.js
@@ -1,9 +1,12 @@
+import { paginationSLideLeft } from "./paginationLogic.js";
+import { paginationSLideRight } from "./paginationLogic.js";
+
 const slideRight = document.querySelector('.carousel__slide_right');
 const slideLeft = document.querySelector('.carousel__slide_left');
 const carousel = document.querySelector('.carousel__container');
 
 export function moveSlider() {
-    let slideRange = 0;
+    let slideRange = -780;
     console.log('work');
     slideRight.addEventListener('click', function() {
         console.log('right');
@@ -13,6 +16,8 @@ export function moveSlider() {
             slideRange = -780;
         }
         carousel.style.left = -slideRange + 'px';
+        paginationSLideRight();
+        console.log(slideRange);
     });
 
     slideLeft.addEventListener('click', function() {
@@ -23,7 +28,8 @@ export function moveSlider() {
             slideRange = 780;
         }
         carousel.style.left = -slideRange + 'px';
+        paginationSLideLeft();
+        console.log(slideRange);
     });
-    console.log(slideRange);
 }
 moveSlider();
