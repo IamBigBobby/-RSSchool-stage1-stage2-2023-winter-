7954bfcc8b60df220e6576c86d52c3498c01a6f3
Denis Svetleishii <denissvetleishii@outlook.com>
2023-12-05 19:47:18 +0400

    feat: add basic desktop pagination

--- coffee-house/src/js/carouselSlider.js
+++ coffee-house/src/js/carouselSlider.js
@@ -1,14 +1,29 @@
 const slideRight = document.querySelector('.carousel__slide_right');
-const slideLeft = document.querySelector('.carousel__slide_left')
+const slideLeft = document.querySelector('.carousel__slide_left');
+const carousel = document.querySelector('.carousel__container');
 
 export function moveSlider() {
+    let slideRange = 0;
     console.log('work');
     slideRight.addEventListener('click', function() {
         console.log('right');
+        slideRange += 780;
+        
+        if (slideRange > 780) {
+            slideRange = -780;
+        }
+        carousel.style.left = -slideRange + 'px';
     });
 
     slideLeft.addEventListener('click', function() {
         console.log('left');
+        slideRange -= 780;
+        
+        if (slideRange < -780) {
+            slideRange = 780;
+        }
+        carousel.style.left = -slideRange + 'px';
     });
+    console.log(slideRange);
 }
 moveSlider();
