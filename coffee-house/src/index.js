5b58c02a454779cf08736cdab3650e3e7acca60a
Denis Svetleishii <denissvetleishii@outlook.com>
2023-12-11 20:48:33 +0400

    feat: add complete tab animation

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -3,3 +3,4 @@ import { moveSlider } from "./js/carouselSlider.js";
 import { touchSlider } from "./js/carouselSwiper.js";
 import { updateProgressBar } from "./js/paginationLogic.js";
 import { getData } from "./js/ModalLogic.js";
+import { moveTag } from "./js/tagsLogic.js";
