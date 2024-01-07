d4ea2ca455d4315233a5db5a1f1b587ec7287311
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-07 17:03:29 +0400

    feat: add base render of keyboard

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,4 +1,8 @@
 import { renderMain, renderGallow } from "./utils/generateField.js";
+import { renderKeyboard, renderWrapperKeyboard } from "./utils/renderKeyboard.js";
 
 renderMain();
 renderGallow();
+
+renderWrapperKeyboard();
+renderKeyboard();
