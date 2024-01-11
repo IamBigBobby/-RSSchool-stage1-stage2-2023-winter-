ef36b101c2601479fb01ac26705ff872ef17e428
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-11 19:31:02 +0400

    fix: fix bug with null input disabledKeys

--- hangman/src/utils/saveData.js
+++ hangman/src/utils/saveData.js
@@ -13,6 +13,11 @@ function saveGameForRealode(
 
   if (storedGameState) {
     gameState = JSON.parse(storedGameState);
+    gameState.answer = answer;
+    gameState.question = question;
+    gameState.countFail = countFail;
+    gameState.loadAswer = loadAswer;
+    gameState.disabledKeys.push(disabledKey);
   } else {
     gameState = {
       answer: answer,
@@ -21,8 +26,8 @@ function saveGameForRealode(
       loadAswer: loadAswer,
       disabledKeys: [],
     };
+    gameState.disabledKeys.push(disabledKey);
   }
-  gameState.disabledKeys.push(disabledKey);
 
   localStorage.setItem('hangmanGameState', JSON.stringify(gameState));
 }
