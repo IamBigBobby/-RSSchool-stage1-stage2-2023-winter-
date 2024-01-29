7e55d01a00a195f166a598a0aaa27d69d0c36fcf
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-29 21:45:18 +0400

    feat: add vertical counting of keys

--- nonograms/src/utils/createField.js
+++ nonograms/src/utils/createField.js
@@ -11,13 +11,13 @@ function createGame() {
   const table = document.createElement("table");
   table.className = "table-nonograms";
 
-  for (let i = 0; i < data.easy[0].length; i++) {
+  for (let i = 0; i < data.easy[1].length; i++) {
     const row = document.createElement("tr");
 
-    for (let j = 0; j < data.easy[0].length; j++) {
+    for (let j = 0; j < data.easy[2].length; j++) {
       const cell = document.createElement("td");
       cell.className = "table-nonograms__cell";
-      if (data.easy[0][i][j] === 1) {
+      if (data.easy[2][i][j] === 1) {
         cell.className = "table-nonograms__cell table-nonograms__cell_answer";
         cell.style.backgroundColor = "black";
       }
@@ -33,29 +33,25 @@ function createGame() {
 }
 
 function countingKeys() {
-  console.log(data.easy[0]);
   const table = document.querySelector(".table-nonograms");
 
-  const leftKeysCell = document.createElement("td");
-  leftKeysCell.className = "table-nonograms__left-keys-cell";
-  const row = table.getElementsByTagName("tr");
-
+  // horizontal counting
   for (let i = 0; i < table.rows.length; i++) {
     const currentRow = table.rows[i];
-    const newCell = leftKeysCell.cloneNode(true);
+    const newCell = document.createElement("td");
+    newCell.className = "table-nonograms__cell_keys-left";
     currentRow.insertBefore(newCell, currentRow.firstChild);
 
     let keys = 0;
 
     for (let j = 0; j < table.rows[i].cells.length; j++) {
-      let cell = table.rows[i].cells[j];
-      let nextCell = table.rows[i].cells[j + 1];
+      const cell = table.rows[i].cells[j];
+      const nextCell = table.rows[i].cells[j + 1];
       if (
         cell.classList.contains("table-nonograms__cell") &&
         cell.classList.contains("table-nonograms__cell_answer")
       ) {
         keys++;
-        console.log(keys);
       }
 
       if (
@@ -66,14 +62,53 @@ function countingKeys() {
         )
       ) {
         if (keys !== 0) {
-          const key = document.createElement("div");
-          key.className = "table-nonograms__cell_answer";
-          key.textContent = keys;
-
-          newCell.appendChild(key);
+          newCell.innerHTML += keys;
           keys = 0;
         }
       }
     }
+
+    if (newCell.textContent === "") {
+      newCell.innerHTML = 0;
+    }
   }
+
+  // vertical counting
+  const newRow = document.createElement("tr");
+
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    const newCell = document.createElement("td");
+    newCell.className = "table-nonograms__cell_keys-top";
+
+    newRow.appendChild(newCell);
+
+    let keys = 0;
+
+    for (let j = 0; j < table.rows.length; j++) {
+      const cell = table.rows[j].cells[i];
+
+      console.log(cell);
+      if (
+        cell.classList.contains("table-nonograms__cell") &&
+        cell.classList.contains("table-nonograms__cell_answer")
+      ) {
+        keys++;
+        if (j === table.rows.length - 1) {
+          if (keys !== 0) {
+            newCell.innerHTML += keys;
+            keys = 0;
+          }
+        }
+      } else {
+        if (keys !== 0) {
+          newCell.innerHTML += keys;
+          keys = 0;
+        }
+      }
+    }
+    if (newCell.textContent === "" && i !== 0) {
+      newCell.innerHTML = 0;
+    }
+  }
+  table.insertBefore(newRow, table.firstChild);
 }
