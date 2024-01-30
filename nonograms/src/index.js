3f2bec00b2229b0a05f41b94580e384c0cee9012
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-30 18:18:14 +0400

    feat: add counting true cells

--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,5 +1,7 @@
 const { createWrapper, createGame } = require("./utils/createField");
 import "./style.scss";
+import { countingTrueCells } from "./utils/countingAnswersCells";
 
 createWrapper();
 createGame();
+countingTrueCells();
