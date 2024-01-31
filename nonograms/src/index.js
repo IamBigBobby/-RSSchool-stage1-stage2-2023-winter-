63a5356af99820178a4c23c9eba4acd3c5abecab
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-31 16:16:16 +0400

    fix: remove counting from index.js

--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,7 +1,6 @@
 const { createWrapper, createGame } = require("./utils/createField");
 import { data } from "./data/dataMatrix";
 import "./style.scss";
-import { countingTrueCells } from "./utils/countingAnswersCells";
 import { createName } from "./utils/createName";
 import { playerLogic } from "./utils/playerLogic";
 
@@ -11,6 +10,5 @@ const firstName = data.easy[0].name;
 createWrapper();
 createGame(firstMatrix);
 createName(firstName);
-countingTrueCells();
 
 playerLogic();
