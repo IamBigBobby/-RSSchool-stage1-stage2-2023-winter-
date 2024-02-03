55f738fb91292dc9f96f3093c9695aa6acb037a2
Denis Svetleishii <denissvetleishii@outlook.com>
2024-02-03 23:16:31 +0400

    feat: add worked win popup

--- nonograms/src/utils/appearingPopUpWin.js
+++ nonograms/src/utils/appearingPopUpWin.js
@@ -1,3 +1,6 @@
+import { playerLogic } from "./playerLogic";
+import { startNewGame } from "./startNewGame";
+
 export { popUpWin };
 
 function popUpWin() {
@@ -5,10 +8,33 @@ function popUpWin() {
   const createPopUpWinBackground = document.createElement("div");
   createPopUpWinBackground.classList = "pop-up-win__background";
 
+  const seconds = getWinTime();
+
   createPopUpWinBackground.innerHTML = `
   <div class="pop-up-win__content">
-  <p>Great! You have solved the nonogram in ## seconds!</p>
+  <p>Great! You have solved the nonogram in ${seconds} seconds!</p>
   </div>
   `;
   document.body.prepend(createPopUpWinBackground);
+
+  const popUpWinBackground = document.querySelector(".pop-up-win__background");
+  popUpWinBackground.addEventListener("mousedown", closePopUp);
+}
+
+function getWinTime() {
+  const winTime = document.querySelector(".nonograms__timer").textContent;
+
+  const minute = Number(winTime.slice(0, 2));
+  const seconds = Number(winTime.slice(3, winTime.length));
+
+  const countingSecond = minute * 60 + seconds;
+  return countingSecond;
+}
+
+function closePopUp() {
+  const popUp = document.querySelector(".pop-up-win__background");
+  popUp.remove();
+
+  startNewGame();
+  playerLogic();
 }
