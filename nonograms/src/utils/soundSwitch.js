d46711b4a6069235a46f6ef8deaa635b847be80b
Denis Svetleishii <denissvetleishii@outlook.com>
2024-02-05 18:11:35 +0400

    fix: fix sound switching after change map

--- nonograms/src/utils/soundSwitch.js
+++ nonograms/src/utils/soundSwitch.js
@@ -5,21 +5,17 @@ function switchSound(event) {
   console.log(event.target.textContent);
   const soundPosition = event.target.textContent;
   if (soundPosition === "sound: off") {
-    const cells = document.querySelectorAll(".table-nonograms__cell");
-    cells.forEach((cell) => {
-      cell.addEventListener("mousedown", leftClickSound);
-      cell.addEventListener("mousedown", rightClickSound);
-    });
+    const wrapper = document.querySelector(".wrapper-nonograms");
+    wrapper.addEventListener("mousedown", leftClickSound);
+    wrapper.addEventListener("mousedown", rightClickSound);
 
     event.target.style.backgroundColor = "aqua";
 
     event.target.innerHTML = "sound: on";
   } else {
-    const cells = document.querySelectorAll(".table-nonograms__cell");
-    cells.forEach((cell) => {
-      cell.removeEventListener("mousedown", leftClickSound);
-      cell.removeEventListener("mousedown", rightClickSound);
-    });
+    const wrapper = document.querySelector(".wrapper-nonograms");
+    wrapper.removeEventListener("mousedown", leftClickSound);
+    wrapper.removeEventListener("mousedown", rightClickSound);
 
     event.target.style.backgroundColor = "white";
 
