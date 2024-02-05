f202b0b4074bd396bd95274603ee5609bfb16ad4
Denis Svetleishii <denissvetleishii@outlook.com>
2024-02-05 16:24:07 +0400

    feat: add sound settings into webpack

--- nonograms/webpack.config.js
+++ nonograms/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -12,6 +15,18 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(wav|mp3|ogg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/sounds/",
+            },
+          },
+        ],
       },
     ],
   },
