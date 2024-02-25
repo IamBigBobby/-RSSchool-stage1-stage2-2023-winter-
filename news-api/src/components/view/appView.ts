8999f83f025980887b3c53e717ca895214311e0b
Denis Svetleishii <denissvetleishii@outlook.com>
2024-02-25 19:19:33 +0400

    refactor: delete console.log into appView.ts

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -12,13 +12,11 @@ export class AppView implements appViewInterface {
   }
 
   drawNews(data: ShowNews) {
-    console.log(data);
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
   drawSources(data: ShowNews) {
-    console.log(data);
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
