eb44f520637fa6fbb8efe87162073cd22e05a7af
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-06 17:29:30 +0400

    feat: add html-webpack-plugin

--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -1,10 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
