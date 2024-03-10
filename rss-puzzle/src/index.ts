41922ffc79c8bef40efd76573a75d507c538a521
Denis Svetleishii <74614852+IamBigBobby@users.noreply.github.com>
2024-03-10 16:36:29 +0400

    feat: add tags farm (#36)

--- rss-puzzle/src/index.ts
+++ rss-puzzle/src/index.ts
@@ -1,12 +1,13 @@
 import { Component } from './componets/componentsFarm';
+import { div } from './componets/tagsFarm';
 
-const newComponent = new Component({
-  tag: 'div',
-  className: 'my-class',
-  text: 'Hello, world!',
-});
+const newDiv = div('new-class')
 
 const body = document.body;
 console.log(body);
 
-body.appendChild(newComponent.getNode());
+body.appendChild(newDiv.getNode());
+
+setTimeout(() => {
+  newDiv.destroy();
+}, 5000);
