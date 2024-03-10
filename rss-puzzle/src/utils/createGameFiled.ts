a0fe26fca006bc0cd0c6fc5133b1ec8ecd82b399
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-10 20:05:50 +0400

    feat: add data and base render for background

--- rss-puzzle/src/utils/createGameFiled.ts
+++ rss-puzzle/src/utils/createGameFiled.ts
@@ -1,14 +1,7 @@
 import { div, img } from '../componets/tagsFarm';
 
 export function createGameFiled(): void {
-  const gameFiled = div(
-    'game-container',
-    img({
-      src: 'https://raw.githubusercontent.com/rolling-scopes-school/rss-puzzle-data/main/images/level1/9th_wave.jpg',
-      alt: '#',
-      className: 'game-container__puzzle',
-    })
-  );
+  const gameFiled = div('game-container');
 
   const mainElement = document.querySelector('main');
 
