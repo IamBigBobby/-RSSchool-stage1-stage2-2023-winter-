9e5ff586ba94c421224a747d8b6b3af36952dd3c
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-10 18:28:03 +0400

    feat: add sass loader

--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -21,7 +21,14 @@ module.exports = {
             }
           }
         ]
-      }
+      },{
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
     ],
   },
   resolve: {
