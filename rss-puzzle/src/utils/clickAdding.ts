e2d1c468ec6d47ae9ca67fdcdd9426a021d08c12
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-14 21:58:24 +0400

    feat: RSS-PZ-10 add adding puzzle back to row-container

--- rss-puzzle/src/utils/clickAdding.ts
+++ rss-puzzle/src/utils/clickAdding.ts
@@ -4,15 +4,22 @@ export function addClickmovementPuzzle(): void {
   const collectingFields: NodeListOf<HTMLElement> = document.querySelectorAll(
     '.collecting-field_active'
   );
+  const rowContainer = document.querySelector('.row-container') as HTMLElement;
 
   puzzleCards.forEach((card) => {
     card.addEventListener('click', (event) => {
       const puzzlePiece = event.target as HTMLElement;
+      const parent = puzzlePiece.parentNode as HTMLElement;
+
+      if (parent.classList.contains('collecting-field')) {
+        parent.removeChild(puzzlePiece);
+        rowContainer.appendChild(puzzlePiece);
+        return;
+      }
 
       for (let i = collectingFields.length - 1; i >= 0; i--) {
         const block = collectingFields[i];
 
-        console.log(block);
         if (!block.querySelector('.puzzle-piece')) {
           puzzlePiece.parentNode.removeChild(puzzlePiece);
           block.appendChild(puzzlePiece);
