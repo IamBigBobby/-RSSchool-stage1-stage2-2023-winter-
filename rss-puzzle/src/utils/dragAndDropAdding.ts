9d1368011ce8363498be58ed6ca6628dcc1530cc
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-15 14:58:10 +0400

    feat: RSS-PZ-15 add drag and drop puzzle from puzzle container to collecting field

--- rss-puzzle/src/utils/dragAndDropAdding.ts
+++ rss-puzzle/src/utils/dragAndDropAdding.ts
@@ -1,10 +1,30 @@
 export function dragAndDropMovementPuzzle(): void {
   const puzzles = document.querySelectorAll('.puzzle-piece');
+  const collectingFields = document.querySelectorAll(
+    '.row-container-collecting_active'
+  );
 
-  console.log(puzzles);
+  let draggedPuzzle: HTMLElement;
+
+  collectingFields.forEach((field) => {
+    field.addEventListener('dragover', (event) => {
+      event.preventDefault();
+    });
+    field.addEventListener('drop', (event) => {
+      event.preventDefault();
+      const dropField = event.target as HTMLElement;
+      if (dropField.classList.contains('collecting-field_active')) {
+        draggedPuzzle.parentNode.removeChild(draggedPuzzle);
+        dropField.appendChild(draggedPuzzle);
+      }
+    });
+  });
+
   puzzles.forEach((puzzle) => {
     puzzle.setAttribute('draggable', 'true');
 
-    puzzle.addEventListener('dragstart', (event) => console.log(event));
+    puzzle.addEventListener('dragstart', (event) => {
+      draggedPuzzle = event.target as HTMLElement;
+    });
   });
 }
