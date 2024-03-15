4a7b9948566849dac48c0896bfec8080c5c48564
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-16 02:06:47 +0400

    feat: RSS-PZ-12 add check button

--- rss-puzzle/src/utils/createGameFiled.ts
+++ rss-puzzle/src/utils/createGameFiled.ts
@@ -1,11 +1,15 @@
-import { div } from '../componets/tagsFarm';
+import { div, main } from '../componets/tagsFarm';
 
 export function createGameFiled(): void {
   const puzzleContainer = div('puzzle-container');
   const fieldCollectingContainer = div('collecting-container');
-
   const mainElement = document.querySelector('main');
+  const button = document.createElement('button');
+  button.classList.add('check-button');
+
+  button.textContent = 'check';
 
   mainElement.appendChild(fieldCollectingContainer.getNode());
+  mainElement.appendChild(button);
   mainElement.appendChild(puzzleContainer.getNode());
 }
