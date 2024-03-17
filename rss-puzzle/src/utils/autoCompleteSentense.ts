bbee82010ef355c142910672c24c35836dfe5520
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-17 14:11:25 +0400

    feat: RSS-PZ-14 add base autocomplete

--- rss-puzzle/src/utils/autoCompleteSentense.ts
+++ rss-puzzle/src/utils/autoCompleteSentense.ts
@@ -1,11 +1,26 @@
-import { round } from './checkSentence';
+import { checkSentence, round } from './checkSentence';
 
 export function autocompleteSentense(textArr: string[][]): void {
   const autoCompleteButton = document.querySelector('.auto-complete');
   autoCompleteButton.addEventListener('click', function () {
-    console.log(textArr[round]);
+    const trueSentense = textArr[round];
     const rowContainerActive = document.querySelector('.row-container_active');
     const children = rowContainerActive.querySelectorAll('.puzzle-piece');
-    console.log(children);
+    const collectingFields = document.querySelectorAll(
+      '.collecting-field_active'
+    );
+    for (let i = 0; i < trueSentense.length; i += 1) {
+      children.forEach((child) => {
+        if (trueSentense[i] === child.textContent) {
+          for (let j = 0; j < collectingFields.length; j += 1) {
+            const blockFiled = collectingFields[i];
+            if (!blockFiled.querySelector('.puzzle-piece')) {
+              blockFiled.appendChild(child);
+            }
+          }
+        }
+      });
+    }
+    checkSentence(textArr);
   });
 }
