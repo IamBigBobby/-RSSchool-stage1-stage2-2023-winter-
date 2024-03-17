decd6bf449f29187b3aeca5774af066e398c7901
Denis Svetleishii <74614852+IamBigBobby@users.noreply.github.com>
2024-03-17 17:12:32 +0400

    Feat:RSS-PZ-14 implement auto complete button (#49)
    * feat: RSS-PZ-14 add base autocomplete
    
    * fix: RSS-PZ-14 fix bugs with double words
    
    * fix: RSS-PZ-14 fix autocomplete dynamic

--- rss-puzzle/src/utils/autoCompleteSentense.ts
+++ rss-puzzle/src/utils/autoCompleteSentense.ts
@@ -1,11 +1,41 @@
-import { round } from './checkSentence';
+import { checkSentence, round } from './checkSentence';
 
 export function autocompleteSentense(textArr: string[][]): void {
   const autoCompleteButton = document.querySelector('.auto-complete');
   autoCompleteButton.addEventListener('click', function () {
-    console.log(textArr[round]);
+    const activeFields = document.querySelectorAll('.collecting-field_active');
+    const rowActive = document.querySelector('.row-container_active');
+    activeFields.forEach((field) => {
+      if (field.querySelector('.puzzle-piece')) {
+        const dragPuzzle = field.querySelector('.puzzle-piece');
+        console.log(dragPuzzle);
+        rowActive.appendChild(dragPuzzle);
+      }
+    });
+
+    const trueSentence = textArr[round];
     const rowContainerActive = document.querySelector('.row-container_active');
     const children = rowContainerActive.querySelectorAll('.puzzle-piece');
-    console.log(children);
+    const collectingFields = document.querySelectorAll(
+      '.collecting-field_active'
+    );
+    children.forEach((child) => {
+      trueSentence.forEach((word, index) => {
+        if (word === child.textContent) {
+          const blockField = collectingFields[index];
+          blockField.appendChild(child.cloneNode(true));
+          while (blockField.children.length > 1) {
+            blockField.removeChild(blockField.lastChild);
+          }
+        }
+      });
+    });
+    const rowContainerActiveChildren = Array.from(rowContainerActive.children);
+
+    rowContainerActiveChildren.forEach((child) => {
+      child.parentNode.removeChild(child);
+    });
+
+    checkSentence(textArr);
   });
 }
