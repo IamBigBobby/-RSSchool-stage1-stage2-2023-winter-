110c78486d8018bf5fc88e533ffb39b3b045964f
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-18 21:49:49 +0400

    feat:RSS-PZ-04 add logout button

---
+++ rss-puzzle/src/utils/logOut.ts
@@ -0,0 +1,13 @@
+import { createInputField } from './createInputField';
+import { destroyPage } from './destroyPage';
+
+export function logOut(): void {
+  const logOutButton = document.querySelector('.button-log-out');
+
+  logOutButton.addEventListener('click', function () {
+    localStorage.removeItem('IamBigBobby_name');
+    localStorage.removeItem('IamBigBobby_surname');
+    destroyPage();
+    createInputField();
+  });
+}
