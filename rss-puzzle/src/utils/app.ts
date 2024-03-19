32638b7c8ab2b9aa3429ff215c467578699806ea
Denis Svetleishii <74614852+IamBigBobby@users.noreply.github.com>
2024-03-19 08:50:28 +0400

    fix:RSS-PZ-80 fix button position (#65)

--- rss-puzzle/src/utils/app.ts
+++ rss-puzzle/src/utils/app.ts
@@ -11,11 +11,14 @@ export function createApp(): void {
   const buttonHintImg = document.createElement('button');
   const buttonHintTranslation = document.createElement('button');
   const logOutButton = document.createElement('button');
+  const buttonContainer = document.createElement('div');
+
   selectLevel.classList.add('selector-lvl');
   selectRound.classList.add('select-round');
   buttonHintImg.classList.add('button-hint-img');
   buttonHintTranslation.classList.add('button-hint-translation');
   logOutButton.classList.add('button-log-out');
+  buttonContainer.classList.add('button-container');
 
   buttonHintImg.textContent = 'Show background: off';
   buttonHintTranslation.textContent = 'Show translate: off';
