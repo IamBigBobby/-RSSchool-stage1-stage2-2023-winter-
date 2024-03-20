5a9c42f6362a5e30f5e1b02576a1fb6e7cc29371
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-20 14:01:39 +0400

    remove rss-puzzle folder

--- rss-puzzle/src/utils/app.ts
+++
@@ -1,7 +0,0 @@
-import { main } from '../componets/tagsFarm';
-
-export function createApp(): void {
-  const app = main('main');
-  const body = document.body;
-  body.appendChild(app.getNode());
-}
