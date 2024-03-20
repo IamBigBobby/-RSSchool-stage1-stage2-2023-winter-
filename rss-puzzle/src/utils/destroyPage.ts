424b27c631e020dc1058fdd169729cd382013607
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-20 21:23:09 +0400

    remove rss-puzzle folder

--- rss-puzzle/src/utils/destroyPage.ts
+++
@@ -1,7 +0,0 @@
-export default function destroyPage(): void {
-  const { body } = document;
-
-  while (body.firstChild) {
-    body.removeChild(body.firstChild);
-  }
-}
--- rss-puzzle/src/utils/firstStart.ts
+++
@@ -1,9 +0,0 @@
-import createApp from './app';
-import createGameField from './createGameFiled';
-import createRound from './createRound';
-
-export default function firstGameStart(): void {
-  createApp();
-  createGameField();
-  createRound(1, 0);
-}
