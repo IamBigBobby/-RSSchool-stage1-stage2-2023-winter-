6ae10cb8e3c4ca64978fb9d5c7d380eb4479bcc1
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-25 19:05:00 +0400

    feat: AR-RSS-01 add pagination request

--- async-race/src/api/getDataGarage.ts
+++ async-race/src/api/getDataGarage.ts
@@ -49,6 +49,23 @@ export default class GarageData {
       });
   }
 
+  public getLimitedGarageData(
+    page: number = 1,
+    limit: number = 2,
+  ): Promise<CarsArray> {
+    const url = `${this.link}/?_page=${page}&_limit=${limit}`;
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP Error");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+
   public deleteCar(carNumber: number): void {
     const deleteUrl = `${this.link}/${carNumber}`;
     const requestOptions = {
--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,17 +1,20 @@
 import "./style.scss";
 import "./assets/car.svg";
-// import GarageData from "./api/getDataGarage";
-import EngineData from "./api/getDataEngine";
+import GarageData from "./api/getDataGarage";
+// import EngineData from "./api/getDataEngine";
 
-// const newDataGarage = new GarageData();
-const newDataEngien = new EngineData();
+const newDataGarage = new GarageData();
+// const newDataEngien = new EngineData();
 
-newDataEngien.switchEngien(1, "started").then((result) => {
+newDataGarage.getLimitedGarageData(1, 7).then((result) => {
   console.log(result);
 });
+// newDataEngien.switchEngien(1, "started").then((result) => {
+//   console.log(result);
+// });
 // newDataGarage.getGarageData().then((result) => {
 //   console.log(result);
 // });
-newDataEngien.driveModeEngien(1).then((result) => {
-  console.log(result);
-});
+// newDataEngien.driveModeEngien(1).then((result) => {
+//   console.log(result);
+// });
