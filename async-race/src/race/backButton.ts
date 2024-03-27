57ca240b82266165519aba8ec1e22476e3779d26
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-27 19:32:34 +0400

    feat:AR-RSS-03 add aboard request after back animation

--- async-race/src/race/backButton.ts
+++ async-race/src/race/backButton.ts
@@ -8,10 +8,18 @@ export default function backCar() {
     button.addEventListener("click", () => {
       console.log("Нажата кнопка возврата с индексом:", index);
 
+      const controller = new AbortController();
+      const { signal } = controller;
+
+      fetch(`http://127.0.0.1:3000/engine/?id=${index + 1}&status=drive`, {
+        signal,
+      });
+
+      controller.abort();
+
       const newEngineData = new EngineData();
 
-      newEngineData.switchEngien(index + 1, "stopped").then((data) => {
-        console.log(data);
+      newEngineData.switchEngien(index + 1, "stopped").then(() => {
         const car = cars[index] as HTMLElement;
         car.classList.remove("car-container_move");
         car.style.animationPlayState = "running";
