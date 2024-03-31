e9943214146068483beb552263bec975afceb584
Denis Svetleishii <denissvetleishii@outlook.com>
2024-03-31 13:37:07 +0400

    feat:AR-RSS-09 add amendment pagination for select button

--- async-race/src/race/selectButton.ts
+++ async-race/src/race/selectButton.ts
@@ -1,4 +1,5 @@
 import GarageData from "../api/getDataGarage";
+import paginationPageAmendment from "../pagination/paginationStatus";
 
 export const idCarSelected: { id: number | undefined } = {
   id: 0,
@@ -16,7 +17,8 @@ export default function selectButton(): void {
 
       const newGarageData = new GarageData();
       newGarageData.getGarageData().then((data) => {
-        const selectedCar = data[index];
+        const selectedCar = data[index + paginationPageAmendment.amendment];
+        console.log(selectedCar);
         const oldName = selectedCar.name;
         const oldColor = selectedCar.color;
         idCarSelected.id = selectedCar.id;
