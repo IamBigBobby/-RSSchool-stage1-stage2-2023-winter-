70ce3b7bf1942b1ff2e67470425f9fa38e18a5c2
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-01 10:53:07 +0400

    feat:AR-RSS-11 add base render winner and race buttons

--- async-race/src/app/app.ts
+++ async-race/src/app/app.ts
@@ -11,16 +11,24 @@ export default function createRace(): void {
   const inputCreateCarContainer = document.createElement("div");
   const inputUpdateContainer = document.createElement("div");
   const generateButton = document.createElement("button");
+  const raceViewButton = document.createElement("button");
+  const winnerViewButton = document.createElement("button");
 
   main.classList.add("main");
 
   inputCreateCarContainer.classList.add("input-create-container");
   inputUpdateContainer.classList.add("input-update-container");
   generateButton.classList.add("button-generate-cars");
+  raceViewButton.classList.add("race-view-button");
+  winnerViewButton.classList.add("winner-view-button");
   generateButton.textContent = "GENERATE";
+  raceViewButton.textContent = "RACE VIEW";
+  winnerViewButton.textContent = "WINNER VIEW";
 
   body.appendChild(inputUpdateContainer);
   body.appendChild(inputCreateCarContainer);
+  body.appendChild(winnerViewButton);
+  body.appendChild(raceViewButton);
   body.appendChild(generateButton);
   body.appendChild(main);
 
