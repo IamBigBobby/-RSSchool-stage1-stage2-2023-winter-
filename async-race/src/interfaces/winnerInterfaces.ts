502ff9b39f4d36f4555c531cf1587a7997b0fa0b
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-01 20:16:58 +0400

    feat:AR-RSS-11 add base render for winning cars

--- async-race/src/interfaces/winnerInterfaces.ts
+++ async-race/src/interfaces/winnerInterfaces.ts
@@ -10,3 +10,12 @@ export interface UpdateWinCar {
   wins: number;
   time: number;
 }
+
+export interface PushWinCar {
+  name: string;
+  time: number;
+  wins: number;
+  color: string;
+}
+
+export interface PushWinCarArr extends Array<PushWinCar> {}
---
+++ async-race/src/winners/destroyViewWinners.ts
@@ -0,0 +1,9 @@
+// export default function destroyViewWinners(): Promise<void> {
+//     return new Promise<void>((resolve) => {
+//       const main = document.querySelector(".winners-page") as HTMLElement;
+//       while (main.firstChild) {
+//         main.removeChild(main.firstChild);
+//       }
+//       resolve();
+//     });
+//   }
