7f9f99877f5877c3e5411002473de285258999ab
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-09 21:26:02 +0400

    feat: add base settings for project

---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'] // Добавляем расширение .ts
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+    }),
+    new EslintPlugin({ extensions: ['ts'] }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
