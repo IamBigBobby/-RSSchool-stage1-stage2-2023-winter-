5ead4addf23aae384ad08be2a28c53469ff0164d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-17 18:42:03 +0400

    feat: add login with using key enter

--- fun-chat/src/utils/buttons/logIn.ts
+++ fun-chat/src/utils/buttons/logIn.ts
@@ -3,5 +3,19 @@ import getUser from "../inputs/getUser";
 export default function logInButtonHandler(
   logInButton: HTMLButtonElement,
 ): void {
+  console.log(logInButton);
   logInButton.addEventListener("click", getUser);
+  document.addEventListener("keydown", (event) => {
+    console.log("слушатель на кнопку повешан");
+    const loginButton = document.querySelector(
+      ".log-in-button",
+    ) as HTMLButtonElement;
+    if (
+      !loginButton.classList.contains("log-in-button_disabled") &&
+      event.key === "Enter"
+    ) {
+      event.preventDefault();
+      getUser();
+    }
+  });
 }
