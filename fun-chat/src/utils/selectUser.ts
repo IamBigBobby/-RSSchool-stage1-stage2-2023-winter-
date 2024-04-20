c25931856ff413cb1ee4daf94a44de95e86206c1
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-20 19:01:45 +0400

    feat: add removing disable status for input and button messenger

--- fun-chat/src/utils/selectUser.ts
+++ fun-chat/src/utils/selectUser.ts
@@ -3,4 +3,14 @@ import destroyWindowChat from "./destroyWindowChat";
 export default function selectUser(event: Event) {
   console.log(event.target);
   destroyWindowChat();
+
+  const messengerInputField = document.querySelector(
+    ".messenger__chat-input",
+  ) as HTMLInputElement;
+  const buttonSendMessenger = document.querySelector(
+    ".messenger__chat-send-button",
+  ) as HTMLButtonElement;
+
+  messengerInputField.classList.remove("messenger__chat-input_disabled");
+  buttonSendMessenger.classList.remove("messenger__chat-send-button_disabled");
 }
