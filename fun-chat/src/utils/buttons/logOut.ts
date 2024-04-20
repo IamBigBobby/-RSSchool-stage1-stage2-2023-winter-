abf7c8e7c022b507471160b1e909b6bdb6020b97
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-20 21:21:36 +0400

    fix: fix double render users after logout

--- fun-chat/src/utils/buttons/logOut.ts
+++ fun-chat/src/utils/buttons/logOut.ts
@@ -1,4 +1,5 @@
 import client from "../../constants/currentClient";
+import message from "../../constants/message";
 // eslint-disable-next-line
 import eventsLoginFiled from "../../render/logIn/eventsLoginField";
 import renderLogInField from "../../render/logIn/renderLoginField";
@@ -10,6 +11,8 @@ export default function logOutButtonHandler(): void {
     const currentElement = event.target as HTMLElement;
     if (currentElement.classList.contains("log-out-button")) {
       console.log("log-out");
+      message.to = "";
+      message.text = "";
       const user = sessionStorage.getItem("userData_iambigbobby") as string;
       const userParse = JSON.parse(user);
       client.logout(userParse.login, userParse.password);
