8a53794a9dbce7ba19e3040574e6fd7f4d599cab
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-21 12:29:52 +0400

    feat: add sending messege with press enter

--- fun-chat/src/utils/buttons/sendMessage.ts
+++ fun-chat/src/utils/buttons/sendMessage.ts
@@ -3,21 +3,68 @@ import message from "../../constants/message";
 import renderSendMessage from "../../render/messenger/renderSendMessage";
 import scrollBottom from "../scrollBottom";
 
-export default function sendMessage() {
-  const sendMessageButton = document.querySelector(
-    ".messenger__chat-send-button",
-  ) as HTMLButtonElement;
+function sendingClick() {
   const inputMessengerField = document.querySelector(
     ".messenger__chat-input",
   ) as HTMLInputElement;
 
-  sendMessageButton.addEventListener("click", () => {
-    if (message.text.trim() !== "") {
-      client.sendingMessage(message.to, message.text);
-      renderSendMessage(message.text);
-      inputMessengerField.value = "";
-      message.text = "";
-      scrollBottom();
-    }
-  });
+  if (message.text.trim() !== "") {
+    client.sendingMessage(message.to, message.text);
+    renderSendMessage(message.text);
+    inputMessengerField.value = "";
+    message.text = "";
+    scrollBottom();
+  }
 }
+
+function sendingEnter(event: KeyboardEvent) {
+  const inputMessengerField = document.querySelector(
+    ".messenger__chat-input",
+  ) as HTMLInputElement;
+
+  const sendMessageButton = document.querySelector(
+    ".messenger__chat-send-button",
+  ) as HTMLButtonElement;
+
+  if (sendMessageButton) {
+    if (
+      !sendMessageButton.classList.contains(
+        "messenger__chat-send-button_disabled",
+      ) &&
+      event.key === "Enter"
+    ) {
+      if (message.text.trim() !== "") {
+        client.sendingMessage(message.to, message.text);
+        renderSendMessage(message.text);
+        inputMessengerField.value = "";
+        message.text = "";
+        scrollBottom();
+      }
+    }
+  }
+}
+
+export default function sendMessage() {
+  const sendMessageButton = document.querySelector(
+    ".messenger__chat-send-button",
+  ) as HTMLButtonElement;
+
+  sendMessageButton.addEventListener("click", sendingClick);
+  document.addEventListener("keydown", sendingEnter);
+}
+
+// document.addEventListener("keydown", (event) => {
+//   const logInButton = document.querySelector(
+//     ".log-in-button",
+//   ) as HTMLButtonElement;
+//   if (logInButton) {
+//     if (
+//       !logInButton.classList.contains("log-in-button_disabled") &&
+//       event.key === "Enter"
+//     ) {
+//       console.log("log-in");
+//       event.preventDefault();
+//       getUser();
+//     }
+//   }
+// });
