b63e1efb853bb15a176022a92214b6a300c1bc36
Denis Svetleishii <denissvetleishii@outlook.com>
2024-04-21 20:52:36 +0400

    fix: fix appearing edit menu

--- fun-chat/src/utils/openEditMenu.ts
+++ fun-chat/src/utils/openEditMenu.ts
@@ -2,8 +2,15 @@ export default function openEditMenu() {
   const editMenu = document.querySelector(".edit-menu") as HTMLElement;
   document.addEventListener("contextmenu", (event) => {
     event.preventDefault();
-    editMenu.style.display = "block";
-    editMenu.style.left = `${event.clientX}px`;
-    editMenu.style.top = `${event.clientY}px`;
+    const currentMessage = event.target as HTMLElement;
+    const closestSendedMessage = currentMessage.closest(
+      ".messenger__sended-message",
+    ) as HTMLElement;
+
+    if (closestSendedMessage) {
+      editMenu.style.display = "block";
+      editMenu.style.left = `${event.clientX}px`;
+      editMenu.style.top = `${event.clientY}px`;
+    }
   });
 }
---
+++ fun-chat/src/utils/selectEditMenu.ts
@@ -0,0 +1,9 @@
+export default function selectEditMenu() {
+  const editButton = document.querySelector(".edit-menu__edit") as HTMLElement;
+  //   const deleteButton = document.querySelector(".edit-menu__delete") as HTMLElement;
+  editButton.addEventListener("click", () => {
+    console.log("click");
+    //   const currentMessage = event.target as HTMLElement;
+    //   if (currentMessage.classList.contains("."))
+  });
+}
